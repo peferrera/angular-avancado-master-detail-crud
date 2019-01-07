fda1c8218035e1b6e11eeaa17b3b3961046bda44
Pedro Ferreira <peferrera@outlook.com>
2019-01-07 02:35:57 -0200

    Improving inMemoryDatabase

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -17,11 +17,11 @@ export class InMemoryDatabase implements InMemoryDbService {
             { id: 1, name: 'Aluguel', categoryId: categories[0].id, category: categories[0], paid: false, date: '14/10/2018', amount: '500,00', type: 'expense', description: 'Aluguel' } as Entry,
             { id: 2, name: 'Uber', categoryId: categories[2].id, category: categories[2], paid: true, date: '14/10/2018', amount: '70,50', type: 'expense', description: 'Uber' } as Entry,
             { id: 3, name: 'Praia', categoryId: categories[2].id, category: categories[2], paid: true, date: '14/10/2018', amount: '50,00', type: 'expense', description: 'Praia' } as Entry,
-            { id: 4, name: 'Salário da Empresa', categoryId: categories[3].id, category: categories[3], paid: true, date: '14/10/2018', amount: '3000,00', type: 'expense', description: 'Salário da Empresa' } as Entry,
+            { id: 4, name: 'Salário da Empresa', categoryId: categories[3].id, category: categories[3], paid: true, date: '14/10/2018', amount: '3000,00', type: 'revenue', description: 'Salário da Empresa' } as Entry,
             { id: 5, name: 'Cinema', categoryId: categories[2].id, category: categories[2], paid: false, date: '14/10/2018', amount: '30,00', type: 'expense', description: 'Cinema' } as Entry,
             { id: 6, name: 'Suplementos', categoryId: categories[1].id, category: categories[1], paid: false, date: '14/10/2018', amount: '100,00', type: 'expense', description: 'Suplementos' } as Entry,
             { id: 7, name: 'Condomínio', categoryId: categories[0].id, category: categories[0], paid: true, date: '14/10/2018', amount: '50,00', type: 'expense', description: 'Condomínio' } as Entry,
-            { id: 8, name: 'Salário da Escola', categoryId: categories[4].id, category: categories[4], paid: true, date: '14/10/2018', amount: '1100', type: 'expense', description: 'Salário da Escola' } as Entry,
+            { id: 8, name: 'Salário da Escola', categoryId: categories[4].id, category: categories[4], paid: true, date: '14/10/2018', amount: '1100,00', type: 'revenue', description: 'Salário da Escola' } as Entry,
             { id: 9, name: 'Festas', categoryId: categories[2].id, category: categories[2], paid: true, date: '14/10/2018', amount: '40,00', type: 'expense', description: 'Festas' } as Entry,
             { id: 10, name: 'Remédios', categoryId: categories[1].id, category: categories[1], paid: false, date: '14/10/2018', amount: '80,00', type: 'expense', description: 'Remédios' } as Entry
         ];
